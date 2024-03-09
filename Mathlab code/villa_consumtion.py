439a4b5dd849db2ce8c1e03ba9f0e3a138677f9d
BentahriMerwane <m.bentahri@students.ephec.be>
2024-03-09 20:48:00 +0100

    add year khw

---
+++ Mathlab code/villa_consumtion.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import csv
+from datetime import datetime
+
+# Initialize lists to store the data
+dates = []
+data_values = []
+
+# Read data from the CSV file
+with open('/home/neo/Downloads/Tekno-Catalay-2024/Mathlab code/kwhwallon.csv', 'r') as file:
+    # Create a CSV reader object
+    reader = csv.reader(file, delimiter=',')
+    
+    # Skip the header row
+    next(reader)
+    
+    # Iterate over each row in the CSV file
+    for row in reader:
+        # Convert the date string to datetime object
+        date = datetime.strptime(row[1], "%Y-%m-%d %H:%M")
+        dates.append(date)  # Append date to the list
+        data_values.append(float(row[-1]))  # Assuming the last column contains the values
+
+# Plot the data
+plt.plot(dates, data_values)
+plt.xlabel('Date')
+plt.ylabel('Value')
+plt.title('Results: Solar photovoltaic power (PV)')
+plt.grid(True)
+plt.show()
